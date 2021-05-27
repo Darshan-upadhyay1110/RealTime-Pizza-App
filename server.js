1afbd52340c28e9b1414b1f4248358239ef90b27
Darshan-Upadhyay <61383886+Darshan-upadhyay1110@users.noreply.github.com>
2021-05-27 16:13:20 +0530

    Added Login , register and cart page

--- server.js
+++ server.js
@@ -8,18 +8,32 @@ const app = express();
 // Assets Config
 app.use(express.static('public'));
 
-//route
-app.get('/',(req,res,next)=>{
-    res.render('home');
-    
-})
+
 
 //Set Templet Engine
 app.use(expressLayouts);
 app.set("views",path.join(__dirname,'/resources/views'));
 app.set("view engine","ejs");
 
+//route
+app.get('/',(req,res,next)=>{
+    res.render('home');
+    
+})
+app.get('/cart',(req,res,next)=>{
+    res.render('customers/cart');
+    
+})
 
+app.get('/login',(req,res,next)=>{
+    res.render('Auth/login');
+    
+})
+
+app.get('/register',(req,res,next)=>{
+    res.render('Auth/register');
+    
+})
 
 
 const PORT = process.env.PORT || 3000
